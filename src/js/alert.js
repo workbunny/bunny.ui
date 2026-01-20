900ce75a41300eed4d4701ad5ceb9272ae9067ec
KingBes <48203677+KingBes@users.noreply.github.com>
2026-01-20 16:39:17 +0800

    优化alert组件

--- src/js/alert.js
+++ src/js/alert.js
@@ -32,7 +32,7 @@ htmx.defineExtension('bny-alert', {
             const color = type(data.code) // 获取颜色
             const anim = data.anim || 'scale' // 获取动画
             const time = data.time || 3 // 获取时间
-
+            
             // 创建alert_open元素
             const alert_open = document.createElement('div')
             alert_open.classList.add('bny-alert-open')
@@ -40,12 +40,13 @@ htmx.defineExtension('bny-alert', {
             const alert = document.createElement('div')
             alert.classList.add('bny-alert', `bny-anim-${anim}`)
             alert.setAttribute('color', color)
+            alert.style.width = 'auto'
             alert.innerHTML = data.msg
             // 将alert添加到alert_open
             alert_open.appendChild(alert)
             // 将alert_open添加到body
             document.body.appendChild(alert_open)
-            // 设置定时器，移除alert_open
+            // 设置定时器，移除alert
             setTimeout(() => {
                 alert_open.remove()
             }, time * 1000)
